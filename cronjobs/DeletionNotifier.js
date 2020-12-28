e4256ab9bea59352fab6e0bf14f8c6bc826ca36a
narberal <nicdanyos@gmail.com>
2020-12-28 21:28:13 +0700

    Optimization

--- cronjobs/DeletionNotifier.js
+++ cronjobs/DeletionNotifier.js
@@ -17,7 +17,7 @@ class DeletionNotifier {
     }
   }
 
-  static async futureDeletionNotifier() {
+  static async getExpiredQuestionnaire(interval) {
     try {
       const queryString = `select distinct users.email from users left join subscription 
       on users.email = subscription.email
@@ -25,10 +25,38 @@ class DeletionNotifier {
       on users.email = questionnaire.email 
         where subscription.email is NULL 
       AND questionnaire_id is not NULL
-        AND created_at <= now() - interval'5 days'`; // change to interval'5 days'
+        AND created_at <= now() - interval`.concat("'", interval, "'"); // change to interval'5 days'
       const queryResult = await db.query(queryString, {
         type: QueryTypes.SELECT,
       });
+      return queryResult;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  static async deleteExpiredQuestionnaire(interval) {
+    try {
+      const deleteQueryString = `delete from questionnaire where questionnaire_id in (
+        select questionnaire_id from users left join subscription 
+          on users.email = subscription.email
+        left join questionnaire 
+          on users.email = questionnaire.email 
+        where subscription.email is NULL 
+          AND questionnaire_id is not NULL
+          AND created_at <= now() - interval`.concat("'", interval, "'", ')');
+      const deleteQueryResult = await db.query(deleteQueryString, {
+        type: QueryTypes.DELETE,
+      });
+      return deleteQueryResult;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  static async futureDeletionNotifier() {
+    try {
+      const queryResult = await DeletionNotifier.getExpiredQuestionnaire('25 mins'); // change to 5 days
       const result = await DeletionNotifier.parallelSender(queryResult, 'futureDeletionNotification');
       console.log(result);
     } catch (error) {
@@ -38,28 +66,10 @@ class DeletionNotifier {
 
   static async deletionNotifier() {
     try {
-      const emailQueryString = `select distinct users.email from users left join subscription 
-      on users.email = subscription.email
-        left join questionnaire 
-      on users.email = questionnaire.email 
-        where subscription.email is NULL 
-      AND questionnaire_id is not NULL
-        AND created_at <= now() - interval'7 days'`; // change to interval'7 days'
-      const deleteQueryString = `delete from questionnaire where questionnaire_id in (
-        select questionnaire_id from users left join subscription 
-          on users.email = subscription.email
-        left join questionnaire 
-          on users.email = questionnaire.email 
-        where subscription.email is NULL 
-          AND questionnaire_id is not NULL
-          AND created_at <= now() - interval'7 days')`; // change to interval'7 days'
-      const emailQueryResult = await db.query(emailQueryString, {
-        type: QueryTypes.SELECT,
-      });
-      const deleteQueryResult = await db.query(deleteQueryString, {
-        type: QueryTypes.DELETE,
-      });
-      const sendResult = await DeletionNotifier.parallelSender(emailQueryResult, 'deletionNotification');
+      const emailQueryResult = await DeletionNotifier.getExpiredQuestionnaire('30 mins'); // change to 7 days
+      const deleteQueryOp = DeletionNotifier.deleteExpiredQuestionnaire('30 mins'); // change to 7 days
+      const sendOp = DeletionNotifier.parallelSender(emailQueryResult, 'deletionNotification');
+      const [deleteQueryResult, sendResult] = await Promise.all([deleteQueryOp, sendOp]);
       console.log(sendResult);
     } catch (error) {
       console.log(error);
--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ pm2 delete Digitrans
+ pm2 delete Digitrans-Worker
+ pm2 delete Digitrans-DeletionNotifier
+ 
  pm2 start --name=Digitrans -i 2 ./api/api.js
  pm2 start --name=Digitrans-Worker -i 2 ./api/worker/EventListener.js
  pm2 start --name=Digitrans-DeletionNotifier ./cronjobs/DeletionNotifier.js --cron="00 00 * * *"
