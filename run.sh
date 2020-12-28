751f3aeb7a8a7e9e0bdf77d043c5aca5171dcbd2
narberal <nicdanyos@gmail.com>
2020-12-28 18:44:40 +0700

    Add cron job to executable script

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 
  pm2 start --name=Digitrans -i 2 ./api/api.js
  pm2 start --name=Digitrans-Worker -i 2 ./api/worker/EventListener.js
+ pm2 start --name=Digitrans-DeletionNotifier ./cronjobs/DeletionNotifier.js --cron="00 00 * * *"
